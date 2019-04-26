0dab95dc18cb1d05fe436a81cb61e1b4bef5cfac
Ewan Burns <ewan@beezup.com>
2019-04-26 17:31:40 +0200

    Sending article through Rabbit

--- apps/generator/article-event-generator/server.js
+++ apps/generator/article-event-generator/server.js
@@ -1,17 +1,24 @@
+let uuid = require("uuid/v1");
 let amqp = require("amqplib");
 
-let endpoint = "amqp://localhost";
+let endpoint = {
+  hostname: "localhost",
+  port: 5672,
+  protocol: "amqp",
+  username: "user",
+  password: "user"
+};
 let queueName = "articles";
 let connection = undefined;
 let channel = undefined;
 
-async function SendAsync() {
+async function SendAsync(articleSummary) {
   try {
     connection = await amqp.connect(endpoint);
     channel = await connection.createChannel();
 
     await channel.assertQueue(queueName, { durable: false });
-    channel.sendToQueue(queueName, Buffer.from("Hello World !"));
+    channel.sendToQueue(queueName, Buffer.from(JSON.stringify(articleSummary)));
   } catch (err) {
     console.log(err);
   } finally {
@@ -25,4 +32,12 @@ async function SendAsync() {
   }
 }
 
-SendAsync();
+SendAsync({
+  id: uuid(),
+  creationUtcDate: Date.now(),
+  modificationUtcDate: Date.now(),
+  author: "Fake Author",
+  title: "Fake Article",
+  publicationUtcDate: Date.now(),
+  description: "Does not actually exist"
+});
--- apps/projector/article-event-elasticsearch-projector/src/index.ts
+++ apps/projector/article-event-elasticsearch-projector/src/index.ts
@@ -1,11 +1,27 @@
 import * as amqp from "amqplib";
 
-let endpoint: string = "amqp://localhost";
+let endpoint: amqp.Options.Connect = {
+  hostname: "localhost",
+  port: 5672,
+  protocol: "amqp",
+  username: "user",
+  password: "user"
+};
 let queueName: string = "articles";
 let connection: amqp.Connection = undefined;
 let channel: amqp.Channel = undefined;
 
-async function SendAsync() {
+class ArticleSummary {
+  id: string;
+  creationUtcDate: Date;
+  modificationUtcDate: Date;
+  author: string;
+  title: string;
+  publicationUtcDate: Date;
+  description: string;
+}
+
+async function ReceiveAsync() {
   try {
     connection = await amqp.connect(endpoint);
     channel = await connection.createChannel();
@@ -14,7 +30,8 @@ async function SendAsync() {
     await channel.consume(
       queueName,
       (msg: amqp.Message) => {
-        console.log(`Received ${msg.content.toString()}`);
+        let articleSummary: ArticleSummary = JSON.parse(msg.content.toString());
+        console.log(articleSummary);
       },
       {
         noAck: true
@@ -25,4 +42,4 @@ async function SendAsync() {
   }
 }
 
-SendAsync();
+ReceiveAsync();
